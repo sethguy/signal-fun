726e7a572c8dbf57707505e9a26acaaeec54d18e
seth <isethguy@gmail.com>
2019-01-08 21:07:31 -0500

    updte to index.js

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
 var signal = require('signal-protocol')
-
-
 var KeyHelper = signal.KeyHelper;
-
 const SignalProtocolStore = require('./utils/InMemorySignalProtocolStore')
 
-const store = new SignalProtocolStore();
-var registrationId = KeyHelper.generateRegistrationId();
 // Store registrationId somewhere durable and safe.
 
-KeyHelper.generateIdentityKeyPair().then(function(identityKeyPair) {
-  // keyPair -> { pubKey: ArrayBuffer, privKey: ArrayBuffer }
-  // Store identityKeyPair somewhere durable and safe.
-  console.log('identityKeyPair', identityKeyPair)
+// KeyHelper.generateIdentityKeyPair().then(function(identityKeyPair) {
+//   // keyPair -> { pubKey: ArrayBuffer, privKey: ArrayBuffer }
+//   // Store identityKeyPair somewhere durable and safe.
 
-  KeyHelper.generatePreKey(registrationId).then(function(preKey) {
-    store.storePreKey(preKey.registrationId, preKey.keyPair);
-    console.log('preKey', preKey)
+//   KeyHelper.generatePreKey(registrationId).then(function(preKey) {
+//     store.storePreKey(preKey.registrationId, preKey.keyPair);
+//     console.log('preKey', preKey)
 
+//     KeyHelper.generateSignedPreKey(identityKeyPair, registrationId).then(function(signedPreKey) {
+//       store.storeSignedPreKey(signedPreKey.keyId, signedPreKey.keyPair);
+//       console.log('signedPreKey', signedPreKey)
+
+//     });
+
+//   });
+// });
+
+
+const generateIdentityKeyPair = new Promise((resolve, reject) => {
+
+  KeyHelper.generateIdentityKeyPair().then(function(identityKeyPair) {
+    resolve(identityKeyPair)
+  });
+
+})
+
+const generatePreKey = (registrationId) => {
+  return new Promise((resolve, reject) => {
+    KeyHelper.generatePreKey(registrationId).then(function(preKey) {
+      store.storePreKey(preKey.registrationId, preKey.keyPair);
+      resolve(preKey)
+    });
+  })
+}
+
+const generateSignedPreKey = (identityKeyPair, registrationId) => {
+  return new Promise((resolve, reject) => {
     KeyHelper.generateSignedPreKey(identityKeyPair, registrationId).then(function(signedPreKey) {
       store.storeSignedPreKey(signedPreKey.keyId, signedPreKey.keyPair);
-      console.log('signedPreKey', signedPreKey)
-
+      resolve(signedPreKey)
     });
-
-  });
-});
-
+  })
+}
 
 
+const init = async() => {
+
+  const store = new SignalProtocolStore();
+  var registrationId = KeyHelper.generateRegistrationId();
+
+  const identityKeyPair = await generateIdentityKeyPair(registrationId)
+
+  const preKey = await generatePreKey(registrationId);
+
+  console.log('preKey', preKey)
+
+  const signedPreKey = await generateSignedPreKey(identityKeyPair, registrationId);
+
+  console.log('signedPreKey', signedPreKey)
+
+}
 
 
+init();
