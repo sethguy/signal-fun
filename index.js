400bc91e59142011b73e55194318be2d9a6368f5
seth tery <isethguy@gmail.com>
2019-01-27 20:45:21 -0500

    using my store for more key stuff

--- index.js
+++ index.js
@@ -70,63 +70,78 @@ const registerClient = async(store) => {
     signedPreKey
   };
 
-  return {deviceId, registrationId};
+  var address = new signal.SignalProtocolAddress(`${registrationId}.1`, deviceId);
+
+  return {deviceId, registrationId,address};
 };
 
-const processPreKeyForUser = async(user, builder) => {
-
-  const sessonConfig = {
-    registrationId: user.registrationId,
-    identityKey: user.identityKeyPair.pubKey,
-    signedPreKey: {
-      keyId: user.signedPreKey.keyId,
-      publicKey: user.signedPreKey.keyPair.pubKey,
-      signature: user.signedPreKey.signature
-    },
-    preKey: {
-      keyId: user.preKey.keyId,
-      publicKey: user.preKey.keyPair.pubKey
-    }
-  }
-
-  await builder.processPreKey(sessonConfig);
-
-}
-
 const init = async() => {
+
   const user1Id = await registerClient(store);
+
   const user2Id = await registerClient(store2);
-  const user1 = myStore[`${user1Id.deviceId}-${user1Id.registrationId}`]
-  const user2 = myStore[`${user2Id.deviceId}-${user2Id.registrationId}`]
-  console.log({user1, user2, myStore});
 
-  var user1Address = new signal.SignalProtocolAddress('33.1', user1.deviceId);
+  var user1ToUser2SessionBuilder = new signal.SessionBuilder(store, user2Id.address);
 
-  var user2Address = new signal.SignalProtocolAddress('44.1', user2.deviceId);
+  const bundle = await generatePreKeyBundle(store2, await store2.getLocalRegistrationId())
 
-  var user1ToUser2SessionBuilder = new signal.SessionBuilder(store, user2Address);
+  await user1ToUser2SessionBuilder.processPreKey(bundle);
 
-  await processPreKeyForUser(user2, user1ToUser2SessionBuilder);
-  
-  var user1Session = new signal.SessionCipher(store, user2Address);
+  var user1Session = new signal.SessionCipher(store, user2Id.address);
 
-  var user2Session = new signal.SessionCipher(store2, user1Address);
+  var user2Session = new signal.SessionCipher(store2, user1Id.address);
 
-  const user1ToUser2Ciphertext = await encryptMsg({msg: "whats if i hada chair", sessionCipher: user1Session})
+  const user1ToUser2Ciphertext = await encryptMsg({msg: "whats if i hada chair", sessionCipher: user1Session});
 
-  console.log(user1ToUser2Ciphertext)
+  console.log('user1ToUser2Ciphertext', user1ToUser2Ciphertext);
 
-  const decrypted = await decryptPreKeyWhisperlMsg({ciphertext: user1ToUser2Ciphertext.body, sessionCipher: user2Session})
+  const decrypted = await decryptPreKeyWhisperlMsg({ciphertext: user1ToUser2Ciphertext.body, sessionCipher: user2Session});
 
-  console.log('decrypted', decrypted)
+  console.log('decrypted', decrypted);
 
-  const user2ToUser1Ciphertext = await encryptMsg({msg: "i gues you coud sit", sessionCipher: user2Session})
-  console.log('TCL: init -> user2ToUser1Ciphertext', user2ToUser1Ciphertext)
+  const user2ToUser1Ciphertext = await encryptMsg({msg: "i gues you coud sit", sessionCipher: user2Session});
 
-  const decrypted2 = await decryptNormalMsg({ciphertext: user2ToUser1Ciphertext.body, sessionCipher: user1Session})
+  const decrypted2 = await decryptNormalMsg({ciphertext: user2ToUser1Ciphertext.body, sessionCipher: user1Session});
 
-  console.log('decrypted2', decrypted2)
+  console.log('decrypted2', decrypted2);
 
 }
 
 init();
+
+const generatePreKeyBundle = (store, registrationId) => {
+  return Promise.all([
+    store.getIdentityKeyPair(),
+      store.getLocalRegistrationId()
+    ])
+    .then(function (result) {
+      var identity = result[0];
+      var registrationId = result[1];
+
+      return Promise.all([
+        KeyHelper.generatePreKey(registrationId),
+          KeyHelper.generateSignedPreKey(identity, registrationId)
+        ])
+        .then(function (keys) {
+          var preKey = keys[0]
+          var signedPreKey = keys[1];
+
+          store.storePreKey(registrationId, preKey.keyPair);
+          store.storeSignedPreKey(registrationId, signedPreKey.keyPair);
+
+          return {
+            identityKey: identity.pubKey,
+            registrationId: registrationId,
+            preKey: {
+              keyId: registrationId,
+              publicKey: preKey.keyPair.pubKey
+            },
+            signedPreKey: {
+              keyId: registrationId,
+              publicKey: signedPreKey.keyPair.pubKey,
+              signature: signedPreKey.signature
+            }
+          };
+        });
+    });
+}
